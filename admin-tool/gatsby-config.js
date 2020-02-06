e5a78ffeb6cf300b3782266c28facf7187094ee9
Jaxcoder <Jaxcoder@outlook.com>
2020-02-06 08:03:45 -0500

    Add etherscan page. Still need to develop out.

--- admin-tool/gatsby-config.js
+++ admin-tool/gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Starter Paperbase Dashboard`,
-		author: `Marc Arbesman`,
-		description: `A starter dashboard demonstrating Material UI's Paperbase theme in Gatsby`,
-		siteUrl: `https://gatsby-starter-paperbase-demo.netlify.com/`,
+		title: `InsureNET Blockchain`,
+		author: `Jason Romero`,
+		description: `A marketplace to buy sell and trade insurance converages and policies.`,
+		siteUrl: `https://insurenet-ico-web.netlify.com/`,
 		social: {
-			twitter: `willcode4food`,
+			twitter: `codenamejason`,
 		},
 	},
 	plugins: [
