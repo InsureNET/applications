da7a6d84b737aaf4776cced5be4b9a0a6799576e
codenamejason <codenamejason@outlook.com>
2020-03-25 20:12:19 -0400

    update whitepaper and move some files

--- admin-tool/truffle-config.js
+++ admin-tool/truffle-config.js
@@ -2,6 +2,8 @@ require('babel-register');
 require('babel-polyfill');
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
+const mnemonic = "outside bridge shrimp above piece myth acquire doll void filter fit reject";
+
 module.exports = {
   networks: {
     development: {
@@ -9,11 +11,22 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    "ropsten-infura": {
-      provider: () => new HDWalletProvider("976b9d6972ea4ba3ba311f823564ed50", "https://ropsten.infura.io/v3/e8cc7c8e245b46b482873ce9382a542b"),
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        "https://ropsten.infura.io/v3/e8cc7c8e245b46b482873ce9382a542b"
+      ),
       network_id: 3,
       gas: 4700000
     },
+    main: {
+      provider: () => new HDWalletProvider(
+        "976b9d6972ea4ba3ba311f823564ed50",
+        "https://mainnet.infura.io/v3/e8cc7c8e245b46b482873ce9382a542b",
+      ),
+      network_id: 5,
+      gas: 4700000
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
