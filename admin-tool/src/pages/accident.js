39e43d8995b35f02b3648e5a3aa3a7f56accd1b8
codenamejason <codenamejason@outlook.com>
2020-03-29 21:43:20 -0400

    add basic accident page

--- admin-tool/src/pages/accident.js
+++ admin-tool/src/pages/accident.js
@@ -2,12 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Layout from 'components/Layout'
 import Content from 'components/Content'
+import AccidentContent from 'components/Accident'
 
 function AccidentPage({ data, location }) {
 	const pageTitle = location ? location.pathname.replace(/\//g, '') : ''
 	return (
 		<Layout location={location} title={pageTitle}>
-			<Content />
+			<AccidentContent />
+		
+			{data}
 		</Layout>
 	)
 }
