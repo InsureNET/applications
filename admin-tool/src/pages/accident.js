48d2ba147329dd99aac2a5a6570155b2c14078de
codenamejason <codenamejason@outlook.com>
2020-04-07 09:10:00 -0400

    fix build errror

--- admin-tool/src/pages/accident.js
+++ admin-tool/src/pages/accident.js
@@ -5,45 +5,45 @@ import Content from 'components/Content'
 import AccidentContent from 'components/Accident'
 import Web3 from 'web3'
 
-async function loadWeb3() {
-	console.group('Web3');
-	console.info('Loading Web3');
-	if (window.ethereum) {
-		window.web3 = new Web3(window.ethereum)
-		await window.ethereum.enable()
-		console.groupEnd();
-	}
-	else if (window.web3) {
-		window.web3 = new Web3(window.web3.currentProvider)
-		console.groupEnd();
-	}
-	else {
-		window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-		console.groupEnd();
-	}
-}
+// async function loadWeb3() {
+// 	console.group('Web3');
+// 	console.info('Loading Web3');
+// 	if (window.ethereum) {
+// 		window.web3 = new Web3(window.ethereum)
+// 		await window.ethereum.enable()
+// 		console.groupEnd();
+// 	}
+// 	else if (window.web3) {
+// 		window.web3 = new Web3(window.web3.currentProvider)
+// 		console.groupEnd();
+// 	}
+// 	else {
+// 		window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+// 		console.groupEnd();
+// 	}
+// }
 
-async function loadBlockchainData() {
-	console.group('Blockchain Data');
-	console.log('loading blockchain data')
-	const web3 = window.web3
-	// Load account
-	const accounts = await web3.eth.getAccounts() 
-	const acct = accounts[0];
-	console.log('blockchain data loaded')
-	console.groupEnd();
-	return acct;
-}
+// async function loadBlockchainData() {
+// 	console.group('Blockchain Data');
+// 	console.log('loading blockchain data')
+// 	const web3 = window.web3
+// 	// Load account
+// 	const accounts = await web3.eth.getAccounts() 
+// 	const acct = accounts[0];
+// 	console.log('blockchain data loaded')
+// 	console.groupEnd();
+// 	return acct;
+// }
 
 function AccidentPage({ data, location, account }) {
 	const pageTitle = location ? location.pathname.replace(/\//g, '') : ''
-	loadWeb3();
-	const accnt = loadBlockchainData();
+	//loadWeb3();
+	//const accnt = loadBlockchainData();
 
 	return (
 		<Layout location={location} title={pageTitle}>
-			<AccidentContent account={account} />		
-			{data}
+			{/* <AccidentContent account={account} />		 
+			{data}*/}
 		</Layout>
 	)
 }
